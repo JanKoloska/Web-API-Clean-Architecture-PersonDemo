7b7c71c885ef6537d91c8483f466ab6b1164b46f
Jan-Hendrik Koloska <134857442+JanKoloska@users.noreply.github.com>
2025-05-18 22:30:48 +0200

    Add unit tests for the Guard class

---
+++ tests/PersonDemo.Core.UnitTests/Utilities/GuardTests.cs
@@ -0,0 +1,41 @@
+﻿using JanKoloska.PersonDemo.Core.Utilities;
+
+namespace JanKoloska.PersonDemo.Core.UnitTests.Utilities;
+
+public class GuardTests
+{
+    [Fact]
+    public void AgainstNull_WithNonNullStringValue_ShouldNotThrow()
+    {
+        // Arrange
+        var value = "Test";
+
+        // Act
+        Guard.AgainstNull(value, nameof(value));
+    }
+
+    [Fact]
+    public void AgainstNull_WithNonNullIntegerValue_ShouldNotThrow()
+    {
+        // Arrange
+        int value = 42;
+
+        // Act
+        Guard.AgainstNull(value, nameof(value));
+    }
+
+    [Fact]
+    public void AgainstNull_WithNullValue_ShouldThrowArgumentNullException()
+    {
+        // Arrange
+        string? value = null;
+
+        // Act
+        var exception = Assert.Throws<ArgumentNullException>(() => Guard.AgainstNull(value, nameof(value)));
+
+        // Assert
+        Assert.NotNull(exception);
+        Assert.Equal(nameof(value), exception.ParamName);
+        Assert.Equal("Parameter 'value' must not be null. (Parameter 'value')", exception.Message);
+    }
+}
