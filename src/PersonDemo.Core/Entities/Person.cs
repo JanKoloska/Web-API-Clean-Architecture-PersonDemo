202c138bb74edec542fa28a5a51f74f96ad6bdaf
Jan-Hendrik Koloska <134857442+JanKoloska@users.noreply.github.com>
2025-05-18 21:03:35 +0200

    Add new entity Person to the solution

---
+++ src/PersonDemo.Core/Entities/Person.cs
@@ -0,0 +1,20 @@
+﻿namespace PersonDemo.Core.Entities;
+
+public class Person
+{
+    public long Id { get; set; }
+
+    public string FirstName { get; set; }
+
+    public string? MiddleName { get; set; }
+
+    public string? LastName { get; set; }
+
+    public DateOnly DateOfBirth { get; set; }
+
+    public Person(string firstName, DateOnly dateOfBirth)
+    {
+        FirstName = firstName;
+        DateOfBirth = dateOfBirth;
+    }
+}
