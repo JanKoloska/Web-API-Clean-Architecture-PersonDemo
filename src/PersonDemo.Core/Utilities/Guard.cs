9710d72b710e2ca394c443539398b79092d0a3ad
Jan-Hendrik Koloska <134857442+JanKoloska@users.noreply.github.com>
2025-05-18 20:46:11 +0200

    Add Guard class with method to throw ArgumentNullException if a passed in parameter is null

---
+++ src/PersonDemo.Core/Utilities/Guard.cs
@@ -0,0 +1,12 @@
+﻿namespace PersonDemo.Core.Utilities;
+
+public static class Guard
+{
+    public static void AgainstNull<T>(T? value, string? parameterName)
+    {
+        if (value is null)
+        {
+            throw new ArgumentNullException(parameterName, $"Parameter '{parameterName}' must not be null.");
+        }
+    }
+}
